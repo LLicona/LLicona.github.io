f9198032818e89e3a6c5722f0cbc397b68e7ab56
LLicona <alicona182@gmail.com>
2022-06-20 07:02:21 -0500

    Update wdc.js

--- wdc.js
+++ wdc.js
@@ -29,24 +29,32 @@
     };
 
     myConnector.getData = function (table, doneCallback) {
-        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-            var feat = resp.features,
+        var url = 'https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json';
+      	$.ajax
+          ({
+            type: "GET",
+            url: url,
+            dataType: 'json',
+            async: false,
+            username: "username",
+            password: "password",
+            success: function (response){
+                var feat = resp.features,
                 tableData = [];
-
-            // Iterate over the JSON object
-            for (var i = 0, len = feat.length; i < len; i++) {
-                tableData.push({
-                    "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
-                });
+                console.log('feat: ',feat);
+                // Iterate over the JSON object
+                for (var i = 0, len = feat.length; i < len; i++) {
+                    tableData.push({
+                        "title": feat[i].title,
+                        "year": feat[i].year,
+                    });
+                }
             }
-
+            
             table.appendRows(tableData);
             doneCallback();
-        });
-
+            
+          });
     };
 
     tableau.registerConnector(myConnector);
